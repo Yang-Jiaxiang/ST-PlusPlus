90acc0deacbc89663dc2b587ff859ba0936817a0
Yang-Jiaxiang <yang900412@gmail.com>
2024-06-17 12:09:29 +0000

    Visulization

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -220,20 +220,7 @@ def train(model, trainloader, valloader, criterion, optimizer, args, step=""):
         for i, (img, mask) in enumerate(tbar):
             img, mask = img.cuda(), mask.cuda()
             pred = model(img)
-            loss = criterion(pred, mask)
-            
-            print('img shape: ', img[0].shape)
-            print('mask shape: ', mask[0].shape)
-            
-            # 檢查 mask 的最小值和最大值
-            mask_min = mask.min().item()
-            mask_max = mask.max().item()
-            print(f'Mask min value: {mask_min}, Mask max value: {mask_max}')
-            
-            # 檢查 mask 中包含的所有不同值及其數量
-            unique_values, counts = torch.unique(mask, return_counts=True)
-            for value, count in zip(unique_values, counts):
-                print(f'Value: {value.item()}, Count: {count.item()}')            
+            loss = criterion(pred, mask)      
             
             # mIou
             pred = torch.argmax(pred, dim=1)            
